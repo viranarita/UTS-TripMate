c1da14f95507d559dd23ed96288b3d65cbe8c822
Savira Narita <viranarita@gmail.com>
2025-03-27 15:36:25 +0700

    coba sidebar dashboard

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,21 +1,50 @@
-<?php
-    session_start();
-?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard | TripMate</title>
+    <title>Document</title>
     <link rel="stylesheet" href="style.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
-<body>
-    <?php include "header.html" ?>
 
-    <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-primary" aria-label="Sidebar">
+<body class="bg-black">
+    <!-- Sidebar Start -->
+    <aside class="bg-white fixed top-0 left-0 h-full w-96 min-w-[384px] shadow-lg flex flex-col z-10">
+
+        <div class="flex flex-col h-full justify-between">
+            
+            <!-- Logo -->
+            <div class="p-7 border-b border-gray-200">
+                <a href="#home" class="font-bold text-lg text-primary">TripMate</a>
+            </div>
+
+            <!-- Menu (Akan Mengisi Ruang Tengah) -->
+            <div class="flex-1">
+                <div class="p-7 space-y-3 border-b border-gray-200">
+                    <a href="#dashboard" class="text-base text-dark hover:text-primary">Coba Deh</a>
+                </div>
+                <div class="p-7 space-y-3 border-b border-gray-200">
+                    <a href="#dashboard" class="text-base text-dark hover:text-primary">Coba Deh</a>
+                </div>
+                <div class="p-7 space-y-3 border-b border-gray-200">
+                    <a href="#dashboard" class="text-base text-dark hover:text-primary">Coba Deh</a>
+                </div>
+            </div>
+
+            <!-- Logout Nempel di Bawah -->
+            <div class="p-7 border-t border-gray-200">
+                <form method="POST" action="prosesLogout.php">
+                    <button type="submit" name="logout" class="text-base text-dark hover:text-primary w-full">Logout</button>
+                </form>
+            </div>
+
+        </div>
 
     </aside>
+
+
+    <!-- Sidebar End -->
 </body>
 </html>
