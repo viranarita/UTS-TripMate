c36d279324c354b94092c06dfae9f302820eabcb
Savira Narita <viranarita@gmail.com>
2025-04-08 13:39:09 +0700

    benerin tailwind

--- public/dashboard.php
+++ public/dashboard.php
@@ -11,35 +11,17 @@
 </head>
 
 <body class="bg-gray-100">
-    
     <?php include "sidebar.html" ?>
     <?php include "headerAdmin.html" ?>
-
     <section class="pt-24 w-full lg:w-[calc(100%-16rem)] lg:ml-64">
-        <div class="flex justify-center mt-4">
-            <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md">
-                <div class="flex justify-between mb-6">
-                    <div class="text-2xl font-semibold mb-1">10</div>
-                    <div class="text-sm font-medium text-gray-400">Active Orders</div>
+        <div class="flex justify-center mt-4 space-x-4">
+            <div class="bg-white rounded-lg border border-gray-100 p-6 shadow-md">
+                <div class="flex justify-center">
+                    <div class="text-4xl font-semibold p-2">10</div>
+                    <div class="text-sm font-medium text-gray-400 p-2 mt-3">Active Orders</div>
                 </div>
-            </div>
-            <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md">
-                <div class="flex justify-between mb-6">
-                    <div class="text-2xl font-semibold mb-1">10</div>
-                    <div class="text-sm font-medium text-gray-400">Active Orders</div>
-                </div>
-            </div>
-            <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md">
-                <div class="flex justify-between mb-6">
-                    <div class="text-2xl font-semibold mb-1">10</div>
-                    <div class="text-sm font-medium text-gray-400">Active Orders</div>
-                </div>
-            </div>
-            <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md">
-                <div class="flex justify-between mb-6">
-                    <div class="text-2xl font-semibold mb-1">10</div>
-                    <div class="text-sm font-medium text-gray-400">Active Orders</div>
-                </div>
+
+                
             </div>
         </div>
     </section>
